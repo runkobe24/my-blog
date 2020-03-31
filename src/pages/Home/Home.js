46803796f512fb6f407563a444666f3efcd38a1d
吴家琪 <648849230@qq.com>
2020-03-31 16:01:14 +0800

    第一次提交

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 const { Header, Content, Footer } = Layout;
 export default function Home() {
+  console.log(5555);
   return (
     <div>
       <p>首页登陆页面</p>
